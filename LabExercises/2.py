655e7056b34ff3fb87421effe35adc7509a844a8
Stefan Stoilkov <stefanstoilkov02@gmail.com>
2022-08-06 15:36:04 +0300

    Update 2.py

--- LabExercises/2.py
+++ LabExercises/2.py
@@ -1,13 +1,11 @@
 '''Da se sastavi programa, koqto izcislqva sumata i 
 proizvedenieto na vsichki chisla ot 1 do dadeno cislo n.'''
 
-n=int(input("Enter n:"))
-sum=0
-p=1
-for i in range(1,n+1):
-    sum+=i
-    p*=i
-print("Sumata e ",sum)
-print("Proizvedenieto e ",p)
-
-
+n = int(input("Enter n:"))
+sum = 0
+p = 1
+for i in range(1, n+1):
+    sum += i
+    p *= i
+print("Sumata e ", sum)
+print("Proizvedenieto e ", p)

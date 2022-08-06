0ae94760122ba1af372aa5b7f9f1e1c7459f53e2
Stefan Stoilkov <stefanstoilkov02@gmail.com>
2022-08-06 15:41:18 +0300

    Update 7.py

--- LabExercises/7.py
+++ LabExercises/7.py
@@ -2,6 +2,6 @@
 na dadeno cislo:'''
 # primer: 76542  da se izvede 24567
 
-num=input("Enter num:")
-for i in range(len(num) -1,-1,-1):
+num = input("Enter num: ")
+for i in range(len(num) -1, -1, -1):
     print(num[i], end='')

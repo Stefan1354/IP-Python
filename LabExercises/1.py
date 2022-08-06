c7ae3398470ae52f37c93ebe6b5ee74f712ee71c
Stefan Stoilkov <stefanstoilkov02@gmail.com>
2022-08-06 15:35:12 +0300

    Update 1.py

--- LabExercises/1.py
+++ LabExercises/1.py
@@ -1,13 +1,13 @@
 '''Da se sastavi programa, koqto namira vsichki chisla,
 v intervala [1,1000], koito zavarshat na 7.'''
 
-#brojac=7
-#while brojac<1001:
-    #print(brojac)
-    #brojac=brojac+10
+brojac = 7
+while brojac<1001:
+    print(brojac)
+    brojac = brojac + 10
 
-#2. nacin
+# second way
 
-# for i in range(1,1000):
-#     if i%10==0:
-#         print(i-3)
+for i in range(1, 1000):
+    if i%10 == 0:
+        print(i-3)

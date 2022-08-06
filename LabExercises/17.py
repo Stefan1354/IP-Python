89266c9da61114e270bd36b12b2f7b8538a90d27
Stefan Stoilkov <stefanstoilkov02@gmail.com>
2022-08-06 16:57:41 +0300

    Update 17.py

--- LabExercises/17.py
+++ LabExercises/17.py
@@ -1,6 +1,6 @@
-'''Da se sastavi programa, koqto vavezda dve celi chisla i 
-izvezda proizvedenieto im samo ako proizvedenieto e
-po-golqmo ot 1000. V protiven slucai da se izvede
+'''Da se sastavi programa, koqto vavejda dve celi chisla i 
+izvejda proizvedenieto im samo ako proizvedenieto e
+po-golqmo ot 1000. V protiven sluchai da se izvede
 tqhnata suma.'''
 
 x = int(input("Enter x: "))

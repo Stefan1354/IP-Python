b0e102158628f7306206d15b8f2449d3321ce4f4
Stefan Stoilkov <stefanstoilkov02@gmail.com>
2022-08-06 15:52:21 +0300

    Update 12.py

--- LabExercises/12.py
+++ LabExercises/12.py
@@ -4,24 +4,24 @@ klaviaturata i namira sumata samo na onezi ot tqh, koito
 sa po golemi ot predvaritelno zadadeno chislo k.
 '''
 
-n=int(input("Enter n: "))
-k=int(input("Enter k: "))
-s=0
+n = int(input("Enter n: "))
+k = int(input("Enter k: "))
+s = 0
 for numbers in range(n):
-    numbers=int(input("Enter n: "))
+    numbers = int(input("Enter n: "))
     if numbers>k:
-        s+=numbers
-print("Sumata e",s)
+        s += numbers
+print("Sumata e", s)
 
-#2 slucai kogato chislata sa po malenki od chisloto k
+#2 sluchai kogato chislata sa po malenki od chisloto k
 
-# n=int(input("Enter n: "))
-# k=int(input("Enter k "))
-# s=0
-# for numbers in range(n):
-#     numbers=int(input("Enter n: "))
-#     s+=numbers
-# if numbers>k:
-#     print(s)
-# else:
-#     print("Vsicki cisla sa po malenki od cisloto k")
+n = int(input("Enter n: "))
+k = int(input("Enter k "))
+s = 0
+for numbers in range(n):
+    numbers = int(input("Enter n: "))
+    s += numbers
+if numbers>k:
+    print(s)
+else:
+    print("Vsicki cisla sa po malenki od cisloto k")

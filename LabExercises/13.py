38f8f7feda2cf3913cc0825e53f8c5ac693d6e81
Stefan Stoilkov <stefanstoilkov02@gmail.com>
2022-08-06 16:38:40 +0300

    Update 13.py

--- LabExercises/13.py
+++ LabExercises/13.py
@@ -3,9 +3,9 @@ ot klaviaturatan i namira sumata samo na onezi ot tqh,
 koito sa po-malki ot predvaritelno zadadeno chislo u 
 i po-golemi ot predvarielno zadadenoto chislo v.'''
 
-n = int(input("Enter n:"))
-u = int(input("Enter u:"))
-v = int(input("Enter v:"))
+n = int(input("Enter n: "))
+u = int(input("Enter u: "))
+v = int(input("Enter v: "))
 s = 0
 for numbers in range (n):
     numbers = int(input("Enter numbers:"))

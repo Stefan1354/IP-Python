816fdd20daa9a7fcf240595b2813f1c88aeda6a6
Stefan Stoilkov <stefanstoilkov02@gmail.com>
2022-08-06 15:37:56 +0300

    Update 5.py

--- LabExercises/5.py
+++ LabExercises/5.py
@@ -1,9 +1,9 @@
 '''Da se sastavi programa, koqto izvezda vsicki prosti cisla
 v diapazona ot 25 do 50.'''
 
-for i in range(25,51):
+for i in range(25, 51):
     if i>0:
-        for a in range(2,i):
+        for a in range(2, i):
             if i%a==0:
                 break
         else:

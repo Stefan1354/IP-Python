5c54d16eb21d219709bbc7b5dafc0b5ce323f5fb
Stefan Stoilkov <stefanstoilkov02@gmail.com>
2022-08-06 16:40:16 +0300

    Update 14.py

--- LabExercises/14.py
+++ LabExercises/14.py
@@ -1,15 +1,15 @@
-'''Da se sastavi programa, koqto vavezhda n na broj chisla
+'''Da se sastavi programa, koqto vavezhda n na broi chisla
 ot klaviaturata i namira srednoaritmeticnoto samo na
 onezi ot tqh, koito sa po-golemi ot predvaritelno
 zadadeno chislo k.'''
 
-n=int(input("Enter n:"))
-k=int(input("Enter k:"))
-s=0
-br=0
+n = int(input("Enter n:"))
+k = int(input("Enter k:"))
+s = 0
+br = 0
 for numbers in range (n):
-    numbers=int(input("Enter numbers:"))
+    numbers = int(input("Enter numbers: "))
     if numbers>k:
-        s+=numbers
-        br+=1
+        s += numbers
+        br += 1
 print(s/br)

56ff9009c9fecbe86a40a50a0a59f040a42615dc
Stefan Stoilkov <stefanstoilkov02@gmail.com>
2022-08-06 15:45:19 +0300

    Update 9.py

--- LabExercises/9.py
+++ LabExercises/9.py
@@ -2,12 +2,12 @@
 klaviaturata i namira srednoaritmetichnata stoinost
 (1<=n<=1000).'''
 
-n=int(input("Enter n:"))
-sum=0
-br=0
+n = int(input("Enter n:"))
+sum = 0
+br = 0
 for numbers in range(n):
-    numbers=int(input("Enter numbers:"))
+    numbers = int(input("Enter numbers:"))
     if n>=1 and n<=1000:
-        sum+=numbers
-        br+=1
-print("Srednoaritmeticnata stoinost e ",sum/br)
+        sum += numbers
+        br += 1
+print("Srednoaritmeticnata stoinost e ", sum/br)

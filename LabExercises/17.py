cc71ba8a10c5146d02c1475dc03fb4a87e408225
Stefan Stoilkov <stefanstoilkov02@gmail.com>
2022-08-06 16:45:51 +0300

    Update 17.py

--- LabExercises/17.py
+++ LabExercises/17.py
@@ -3,11 +3,11 @@ izvezda proizvedenieto im samo ako proizvedenieto e
 po-golqmo ot 1000. V protiven slucai da se izvede
 tqhnata suma.'''
 
-x=int(input("Enter x: "))
-y=int(input("Enter y: "))
-z=x*y
-b=x+y
+x = int(input("Enter x: "))
+y = int(input("Enter y: "))
+z = x * y
+b = x + y
 if z>1000:
-    print("Proizvedenieto e:", z)
+    print("Proizvedenieto e: ", z)
 else:
-    print("Sumata e",b)
+    print("Sumata e: ",b)

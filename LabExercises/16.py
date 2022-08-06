e6ec6f6868e320acc794e1069641b8a235c1f199
Stefan Stoilkov <stefanstoilkov02@gmail.com>
2022-08-06 16:45:07 +0300

    Update 16.py

--- LabExercises/16.py
+++ LabExercises/16.py
@@ -1,18 +1,18 @@
-'''Da se sastavi programa, koqto vavezda n na broj chisla ot
+'''Da se sastavi programa, koqto vavezda n na broi chisla ot
 klaviaturata i namira srednoaritmeticnata stoinost na 
 vsicki cetni cisla i proizvedenieto na vsichki necetni
 cisla.'''
 
-n=int(input("Enter n:"))
-s=0
-br=0
-p=1
+n = int(input("Enter n: "))
+s = 0
+br = 0
+p = 1
 for numbers in range(n):
-    numbers=int(input("Enter numbers:"))
+    numbers = int(input("Enter numbers: "))
     if numbers %2==0:
-        s+=numbers
-        br+=1
+        s += numbers
+        br += 1
     else:
-        p*=numbers
+        p *= numbers
 print("Srednoaritmeticnata stoinost na vsicki cetni cisla e", s/br)
 print("Proizvedenieto na vsicki necetni cisla e", p)

ded64cbc2c2a0521c87e55fe2f02a7d87b150250
Stefan Stoilkov <stefanstoilkov02@gmail.com>
2022-08-08 15:35:58 +0300

    Add files via upload

---
+++ LabExercises/19.10.py
@@ -0,0 +1,35 @@
+'''1.Da se sastavi programa, koqto vavezda n celi cisla,
+i gi sumira.'''
+
+# n=int(input("Enter n "))
+# sum=0
+# for i in range(n):
+#     num=int(input("Enter num "))
+#     sum=sum+num
+# print("Sumata e",sum)
+
+
+'''2.Da se sastavi programa, koqto vavezda cislo n
+i pecati triugulnik ot zvezdicki.'''
+
+#n=int(input("Enter n "))
+#for i in range(1,n+1):
+    #print('*'*i)
+
+'''3.Da se sastavi programa koqto proverqva da li
+cisloto e prosto, koeto vavezda potrebitelq.'''
+
+# n=int(input("Enter n "))
+# isPrime=False
+# if n<2:
+#     isPrime=False
+# else:
+#     for i in range(2,n):
+#         if n%i==0:
+#             isPrime=True
+#             break
+# if isPrime:
+#     print("NotPrime")
+# else:
+#      print("Prime")
+

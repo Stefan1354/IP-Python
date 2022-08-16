d3ec9c5f7600e460c5323dea48f5c17aa667936d
Stefan Stoilkov <stefanstoilkov02@gmail.com>
2022-08-16 16:43:25 +0300

    Update zadaciyt31-40.py

--- OtherExercises/zadaciyt31-40.py
+++ OtherExercises/zadaciyt31-40.py
@@ -232,70 +232,70 @@ else:
 
 '''1. nacin'''
 
-# year = int(input("Input a year: "))
+year = int(input("Input a year: "))
 
-# if (year % 400 == 0):
-#     leap_year = True
-# elif (year % 100 == 0):
-#     leap_year = False
-# elif (year % 4 == 0):
-#     leap_year = True
-# else:
-#     leap_year = False
+if (year % 400 == 0):
+    leap_year = True
+elif (year % 100 == 0):
+    leap_year = False
+elif (year % 4 == 0):
+    leap_year = True
+else:
+    leap_year = False
 
-# month = int(input("Input a month [1-12]: "))
+month = int(input("Input a month [1-12]: "))
 
-# if month in (1, 3, 5, 7, 8, 10, 12):
-#     month_length = 31
-# elif month == 2:
-#     if leap_year:
-#         month_length = 29
-#     else:
-#         month_length = 28
-# else:
-#     month_length = 30
+if month in (1, 3, 5, 7, 8, 10, 12):
+    month_length = 31
+elif month == 2:
+    if leap_year:
+        month_length = 29
+    else:
+        month_length = 28
+else:
+    month_length = 30
 
 
-# day = int(input("Input a day [1-31]: "))
+day = int(input("Input a day [1-31]: "))
 
-# if day < month_length:
-#     day += 1
-# else:
-#     day = 1
-#     if month == 12:
-#         month = 1
-#         year += 1
-#     else:
-#         month += 1
-# print("The next date is [yyyy-mm-dd] %d-%d-%d." % (year, month, day))
+if day < month_length:
+    day += 1
+else:
+    day = 1
+    if month == 12:
+        month = 1
+        year += 1
+    else:
+        month += 1
+print("The next date is [yyyy-mm-dd] %d-%d-%d." % (year, month, day))
 
 
 '''2. nacin'''
 
-# day, month, year = map(int, input("Enter date: ").split('/'))
+day, month, year = map(int, input("Enter date: ").split('/'))
  
-# if month == 2:
-#     if year % 4 != 0:
-#         d_max = 28
-#     else:
-#         d_max = 29
+if month == 2:
+    if year % 4 != 0:
+        d_max = 28
+    else:
+        d_max = 29
          
-# elif month in [4, 6, 9, 11]:
-#     d_max = 30
+elif month in [4, 6, 9, 11]:
+    d_max = 30
  
-# else:
-#     d_max = 31
+else:
+    d_max = 31
  
-# if 1 <= day <= d_max and 1 <= month <= 12:
-#     if day == d_max:
-#         day = 1
-#         if month == 12:
-#             month = 1
-#             year += 1
-#         else:
-#             month += 1
-#     else:
-#         day += 1
-#     print(str(day) + "/" + str(month) + "/" + str(year))
-# else:
-#     print("Invalid date")
+if 1 <= day <= d_max and 1 <= month <= 12:
+    if day == d_max:
+        day = 1
+        if month == 12:
+            month = 1
+            year += 1
+        else:
+            month += 1
+    else:
+        day += 1
+    print(str(day) + "/" + str(month) + "/" + str(year))
+else:
+    print("Invalid date")

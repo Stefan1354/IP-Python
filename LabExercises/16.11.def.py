688196eba900212b189cde4e434c1cf90bef3a05
Stefan Stoilkov <stefanstoilkov02@gmail.com>
2022-08-16 15:14:42 +0300

    Add files via upload

---
+++ LabExercises/16.11.def.py
@@ -0,0 +1,16 @@
+#funckiq za dlzina na argument: len()
+a=['foo','bar','baz','qux']
+len(a)
+print(len(a))  #4
+
+
+'''def <function_name>([parameters>]):
+    <statement(s)>
+<function_name>([arguments>])'''
+
+def f(x):
+    if x<0:
+        return
+    if x>100:
+        return
+        print(x)

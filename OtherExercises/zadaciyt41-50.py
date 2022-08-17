114d61a05c3eeb3bf92fac5a3f504cc6fcd5b585
Stefan Stoilkov <stefanstoilkov02@gmail.com>
2022-08-18 01:18:27 +0300

    Update zadaciyt41-50.py

--- OtherExercises/zadaciyt41-50.py
+++ OtherExercises/zadaciyt41-50.py
@@ -1,8 +1,8 @@
-'''41.Data su 2 datuma u obliku(d.m.g).Ispisati datum koji
+'''41.Data su 2 datuma u obliku(d.m.g). Ispisati datum koji
 je pre (stariji).'''
 
-d1, m1, g1 = map(int,input("Unesi 1. datum ").split('.'))
-d2, m2, g2 = map(int,input("Unesi 2. datum ").split('.'))
+d1, m1, g1 = map(int,input("Unesi 1. datum: ").split('.'))
+d2, m2, g2 = map(int,input("Unesi 2. datum: ").split('.'))
 if g1<g2:
     print(d1,'.',m1,'.',g1)
 elif g2<g1:
@@ -21,8 +21,8 @@ else:
             print("Datumi su isti")
 
 #2.nacin
-d1,m1,g1=map(int,input("Unesite 1. datum ").split('.'))
-d2,m2,g2=map(int,input("Unesite 2.datum ").split('.'))
+d1,m1,g1 = map(int,input("Unesite 1. datum: ").split('.'))
+d2,m2,g2 = map(int,input("Unesite 2.datum: ").split('.'))
   #15.02.2020
   #20200215=2020*10000+2*100+15
 
@@ -35,14 +35,14 @@ elif dat2<dat1:
 else:
     print("Datumi su isti")
 
-'''42.Dat je datum(dan,mesec,godina).Ispisati da li je ispravan
+'''42.Dat je datum(dan,mesec,godina). Ispisati da li je ispravan
 ili ne.'''
 
-d, m, g = map(int,input("Unesite dan,mesec i godinu (d.m.g) ").split('.'))
+d, m, g = map(int,input("Unesite dan,mesec i godinu (d.m.g): ").split('.'))
 if m<1 or m>12:
     print("Datum nije ispravan")
 else:
-    broj_dana=31
+    broj_dana = 31
     if m==4 or m==6 or m==9 or m==11:
         broj_dana=30
     elif m==2:
@@ -68,7 +68,7 @@ else:
     print('ne')
 
 '''44.Svake godine se odrzavaju 4 grend slem turnira:otvoreno
-prvenstvo Australije,Francuske,Engleske,SAD.Ako se znaju
+prvenstvo Australije,Francuske,Engleske,SAD. Ako se znaju
 pobednici proslogodisnjih turnira,napisi program koji 
 proverava da li je Nadal i te godine uspeo da osvoji
 otvoreno prvenstvo Franuske(ako jeste ispisati da,a u suprotnom ne).'''
@@ -83,7 +83,7 @@ else:
     print('ne')
 
 '''45.Leto traje od 20.juna do 22.septembra(pretpostavimo da su
-u oba datuma ukljucena leto).Napisi program koji za uneti datum
+u oba datuma ukljucena leto). Napisi program koji za uneti datum
 ispisuje da li je tokom leta i ispisuje da tj. ne.'''
 
 dan = int(input("Unesite dan: "))
@@ -95,10 +95,10 @@ else:
     print('ne')
 
 '''46.Joca je kupio poklon za rodjendan mladjem bratu i
-starijoj sestri.Napisi program koji proverava da li je
+starijoj sestri. Napisi program koji proverava da li je
 razlika u ceni ta dva poklona veca od 1000 dinara(tada Joca
-treba da dokupi jos nesto,da se onaj ko je dobio jeftiniji
-poklon ne bi osetio povredjenim).Ako je razlika mala, ispisati
+treba da dokupi jos nesto, da se onaj ko je dobio jeftiniji
+poklon ne bi osetio povredjenim). Ako je razlika mala, ispisati
 u redu, a u suprotnom ispisati nije u redu.
 Osiguraj da tvoj program radi i u slucaju kada je brat
 dobio skuplji poklon i kada je sestra dobila skuplji poklon.'''
@@ -113,7 +113,7 @@ else:
 
 '''47.Na dopunsku nastavu iz matematike treba da idu ucenici
 cija je prosecna ocena na prva tri kontrolna zadatka strogo
-manja od 3.Napisi program koji ispisuje da ili ne u
+manja od 3. Napisi program koji ispisuje da ili ne u
 zavisnosti od toga da li Djordje treba da ide na dopunsku,
 ako su poznate njegove tri ocene iz matematike.'''
 
@@ -126,7 +126,7 @@ else:
     print('ne')
 
 '''48.Svake goodine se odrzavaju 4 grend slem turnira:otvoreno
-prvenstvo Australije,Francuske,Engleske i SAD.Ako se znaju
+prvenstvo Australije, Francuske, Engleske i SAD. Ako se znaju
 pobednici proslogodisnjih turnira,napisi program koji 
 proverava da li je Novak Djokovic uspeo da osvoji sva 4
 (ako jeste ispisati da,a u suprotnom ne).'''

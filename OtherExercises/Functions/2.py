b15af5eff8c58f273a87977b5129698da7269e55
Stefan Stoilkov <stefanstoilkov02@gmail.com>
2022-08-22 13:07:37 +0300

    Update 2.py

--- OtherExercises/Functions/2.py
+++ OtherExercises/Functions/2.py
@@ -3,15 +3,15 @@
 #в който са включени само четните числа от списъка аргумент.
 
 def cetni(l):
-    new_list=[]
+    new_list = []
     for i in range (len(l)):
         if l[i]%2==0:
             new_list.append(l[i])
     return new_list
 
-n=int(input("Enter n "))
+n = int(input("Enter n: "))
 nums=[]
 for i in range(n):
-    num=int(input(f"Please enter number {i+1}: "))
+    num = int(input(f"Please enter number {i+1}: "))
     nums.append(num)
 print(cetni(nums))

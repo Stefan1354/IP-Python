7ea99a625fba618b528a487861c45e72219d4aec
Stefan Stoilkov <stefanstoilkov02@gmail.com>
2022-08-23 12:43:55 +0300

    Update 30.11.py

--- LabExercises/30.11.py
+++ LabExercises/30.11.py
@@ -6,18 +6,18 @@
 
 '''1 nacin'''
 
-# class Person:
-#     def __init__ (self,name,family,age,nationality):
-#         self.name=name
-#         self.family=family
-#         self.age=age
-#         self.nationality=nationality
-# MyPerson=Person("Stefan","Stoilkov","Bulgarian",19)
-# print(MyPerson)
-# print(MyPerson.name)
-# print(MyPerson.family)
-# print(MyPerson.age)
-# print(MyPerson.nationality)       #printira
+class Person:
+    def __init__ (self, name, family, age, nationality):
+        self.name = name
+        self.family = family
+        self.age = age
+        self.nationality = nationality
+MyPerson=Person("Stefan","Stoilkov","Bulgarian",19)
+print(MyPerson)
+print(MyPerson.name)
+print(MyPerson.family)
+print(MyPerson.age)
+print(MyPerson.nationality)         #printira
                                     #Stefan
                                     #Stoilkov
                                     #Bulgarian
@@ -26,29 +26,29 @@
 
 '''2 nacin'''
 
-# class Person:
-#     def __init__(self,name,family,age,nationality):
-#         self.name=name
-#         self.family=family
-#         self.age=age
-#         self.nationality=nationality
-# p1=Person(name=input("Enter name: "), family=input("Enter family: "), age=input("Enter age: "), nationality=input("Enter nationality: "))
-# print(p1.name, p1.family, p1.age, p1.nationality)   #printira Stefan Stoilkov 19 Bulgarian
+class Person:
+    def __init__(self, name, family, age, nationality):
+        self.name = name
+        self.family = family
+        self.age = age
+        self.nationality = nationality
+p1 = Person(name=input("Enter name: "), family=input("Enter family: "), age=input("Enter age: "), nationality=input("Enter nationality: "))
+print(p1.name, p1.family, p1.age, p1.nationality)   #printira Stefan Stoilkov 19 Bulgarian
 
 
 '''3 nacin'''
 
-# class Person:
-#     def __init__(self,name,family,age,nationality):
-#         self.name=name
-#         self.family=family
-#         self.age=age
-#         self.nationality=nationality
+class Person:
+    def __init__(self, name, family, age, nationality):
+        self.name = name
+        self.family = family
+        self.age = age
+        self.nationality = nationality
     
-#     def __str__(self):
-#         return f'{self.name} {self.family} is {self.age} age and {self.nationality} nationality '
-# person1=Person('Stefan','Stoilkov',19,'Bulgarian')
-# print(person1)  #printira Stefan Stoilkov is 19 age and Bulgarian nationality
+    def __str__(self):
+        return f'{self.name} {self.family} is {self.age} age and {self.nationality} nationality '
+person1 = Person('Stefan','Stoilkov',19,'Bulgarian')
+print(person1)  #printira Stefan Stoilkov is 19 age and Bulgarian nationality
 
 
 
@@ -59,43 +59,43 @@
 
 '''1 nacin'''
 
-# class Student:
-#     def __init__(self, student_id, student_name):
-#         self.student_id=student_id
-#         self.student_name=student_name
+class Student:
+    def __init__(self, student_id, student_name):
+        self.student_id = student_id
+        self.student_name = student_name
 
-#     def add_class(self, student_class):
-#         self.student_class=student_class
+    def add_class(self, student_class):
+        self.student_class = student_class
 
-#     def Print(self):
-#         print(self.student_name, self.student_id, self.student_class)
+    def Print(self):
+        print(self.student_name, self.student_id, self.student_class)
 
-# student1=Student(1212121, "Stefan")
-# student1.add_class(43)
-# student1.Print()                         #printira Stefan 1212121 43
+student1 = Student(1212121, "Stefan")
+student1.add_class(43)
+student1.Print()                         #printira Stefan 1212121 43
 
 
 '''2 nacin'''
 
-# class Student:
-#     def __init__(self,name,id):
-#         self.name=name
-#         self.id=id
+class Student:
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id
     
-#     def __add__(self,student_class):
-#         self.student_class=student_class
-#     def print(self):
-#         print(self.name)
-#         print(self.id)
-#         print(self.student_class)
+    def __add__(self, student_class):
+        self.student_class = student_class
+    def print(self):
+        print(self.name)
+        print(self.id)
+        print(self.student_class)
 
-# p1=Student("Ivan", 221)
-# p2=Student("Mitko", 222)
-# p1.__add__("Mathematics")
-# p2.__add__("Physics")
-# p1.print()
-# print()
-# p2.print()                #printira
+p1=Student("Ivan", 221)
+p2=Student("Mitko", 222)
+p1.__add__("Mathematics")
+p2.__add__("Physics")
+p1.print()
+print()
+p2.print()                  #printira
                             #Ivan
                             #221
                             #Mathematics
@@ -106,13 +106,13 @@
 
 '''3 nacin'''
 
-# class Student:
-#     def __init__(self,Student_ID,Student_name):
-#         self.Student_ID=Student_ID
-#         self.Student_name=Student_name
-#         self.Student_class=None
-#     def get_info(self):
-#         return f'Student_ID {self.Student_ID} with Student_name {self.Student_name} is in the class {self.Student_class}'
+class Student:
+    def __init__(self, Student_ID, Student_name):
+        self.Student_ID = Student_ID
+        self.Student_name = Student_name
+        self.Student_class = None
+    def get_info(self):
+        return f'Student_ID {self.Student_ID} with Student_name {self.Student_name} is in the class {self.Student_class}'
 
-# student1=Student(16423,'Stefan')
-# print(student1.get_info())    #printira Student_ID 16423 with Student_name Stefan is in the class None
+student1 = Student(16423,'Stefan')
+print(student1.get_info())    #printira Student_ID 16423 with Student_name Stefan is in the class None

61ffd2dbafc75ecd784d6daf12cd3c6eb4fef618
Stefan Stoilkov <stefanstoilkov02@gmail.com>
2022-08-24 08:56:40 +0300

    Add files via upload

---
+++ OtherExercises/Functions/4.py
@@ -0,0 +1,15 @@
+#4.	Да се състави програма с функция с един текстов аргумент и произволен брой целочислени
+#аргументи. Резултатът се явява текст, сформиран от буквите на първия текстов аргумент. 
+#Целочислените аргументи определят индексите на буквите, които трябва да влязат в текста 
+#резултат.
+
+def string_index(*args, string):
+    new_string = ""
+    for index in args:
+        if index < len(string) and index >= 0:
+            new_string += string[index]
+
+    return new_string
+
+
+print(string_index(1, 3, 3, 0, 0, string='Hello World'))
---
+++ OtherExercises/Functions/5.py
@@ -0,0 +1,22 @@
+#5.	Да се състави програма с функция, която получава като аргументи референция към функция и две цели числа. Функцията връща като резултат
+#най-голямата стойност на предадената като първи аргумент функция в целочислените точки на диапазона, границите на които се определят от втория
+#и третия аргумент.
+
+def max_s(func='x*2',a=1,b=4):
+    max = eval(func.replace('x', str(a)))
+    for i in range(a,b+1):
+        value = eval(func.replace('x', str(i)))
+        if value > max:
+            max = value
+        print(f"f({i})={eval(func.replace('x', str(i)))}")
+    return max
+
+
+
+function = input("Type in a function (eg. x*3):\n")
+
+lim_1 = int(input("Type in the left limit: "))
+lim_2 = int(input("Type in the right limit: "))
+
+print(f"The highest value of the function ({function}) in the limit of [{lim_1}, {lim_2}] ="
+      f" {max_s(function, lim_1, lim_2)}")

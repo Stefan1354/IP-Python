e591edf3b9ee64b507520e808a4259c905064fdf
Stefan Stoilkov <stefanstoilkov02@gmail.com>
2022-08-24 08:59:04 +0300

    Update 6.py

--- OtherExercises/Functions/6.py
+++ OtherExercises/Functions/6.py
@@ -1,23 +1,23 @@
-#6.	Напишете програма, в която по метода на рекурсията символите от текста, предаден като аргумент на функцията, се изобразяват „през един“: 
+#6.Напишете програма, в която по метода на рекурсията символите от текста, предаден като аргумент на функцията, се изобразяват „през един“: 
 #т.е. изписва се първият, третият, петият и т.н. символ.
 
-def recursion(string, index=0):
+def recursion(string, index = 0):
     if index > len(string) - 1:
         return None
     if index % 2 == 0:
         print(string[index])
     recursion(string, index+1)
-recursion(string="Hello World")
+recursion(string = "Hello World")
 
 
 #2 nacin
 
-# def recursion(string):
-#     new_string=''
-#     for i in range(len(string)):
-#         if i%2==0:
-#             new_string+=string[i]
-#     return new_string
-# string=input("Enter string: ")
-# print(recursion(string))        #Enter string: Stefan
-                                  #Sea
+def recursion(string):
+    new_string = ''
+    for i in range(len(string)):
+        if i%2==0:
+            new_string+=string[i]
+    return new_string
+string = input("Enter string: ")
+print(recursion(string))        #Enter string: Stefan
+                                #Sea

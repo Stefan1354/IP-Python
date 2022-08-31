e335b21065fdd1864399b5ee1f84e02d53fff338
Stefan Stoilkov <stefanstoilkov02@gmail.com>
2022-08-31 18:56:15 +0300

    Update triangle.py

--- LabExercises/LiceNaGeomFigura/triangle.py
+++ LabExercises/LiceNaGeomFigura/triangle.py
@@ -1,6 +1,6 @@
 def triangle():
     print("=======\ntriangle")
-    a=int(input("Enter a "))
-    ha=int(input("Enter ha "))
-    S=a*ha/2
+    a = int(input("Enter a: "))
+    ha = int(input("Enter ha: "))
+    S = a * ha / 2
     print(f"S={S}")

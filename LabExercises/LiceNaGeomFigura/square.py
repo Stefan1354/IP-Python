0bd3ca8b28966c0711b2bcb93721def348ac9c04
Stefan Stoilkov <stefanstoilkov02@gmail.com>
2022-08-31 18:53:08 +0300

    Add files via upload

---
+++ LabExercises/LiceNaGeomFigura/square.py
@@ -0,0 +1,5 @@
+def square():
+    print("=======\nsquare")
+    a=int(input("Enter a "))
+    S=a*a
+    print(f"S={S}")
---
+++ LabExercises/LiceNaGeomFigura/trapezoid.py
@@ -0,0 +1,7 @@
+def trapezoid():
+    print("=======\ntrapezoid")
+    a=int(input("Enter a: "))
+    b=int(input("Enter b: "))
+    h=int(input("Enter h: "))
+    S=((a+b)/2)*h
+    print(f"S={S}")

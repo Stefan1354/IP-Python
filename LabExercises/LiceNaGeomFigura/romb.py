b27730467ea3656755d960d531f5213b5e3e2d80
Stefan Stoilkov <stefanstoilkov02@gmail.com>
2022-08-31 18:54:49 +0300

    Update romb.py

--- LabExercises/LiceNaGeomFigura/romb.py
+++ LabExercises/LiceNaGeomFigura/romb.py
@@ -1,6 +1,6 @@
 def romb():
     print("=======\nromb")
-    d1=int(input("Enter d1: "))
-    d2=int(input("Enter d2: "))
-    S=d1*d2/2
+    d1 = int(input("Enter d1: "))
+    d2 = int(input("Enter d2: "))
+    S = d1 * d2 / 2
     print(f"S={S}")

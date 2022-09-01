8a2f1a6e5012c3e76859e253ba95f867c47b0a0b
Stefan Stoilkov <stefanstoilkov02@gmail.com>
2022-09-01 13:37:52 +0300

    Add files via upload

---
+++ OtherExercises/zadnet.py
@@ -0,0 +1,33 @@
+'''1.Write a Python program to calculate the sum and average of n integer numbers (input from the user). 
+Input 0 to finish.'''
+
+# n=int(input("Enter n: "))
+# sum=0
+# br=0
+# for i in range(1,n+1):
+#     num=int(input("Enter num: "))
+#     sum=sum+num
+#     br=br+1
+#     avg=sum/br
+# print("Sum is: ",sum)
+# print("Average is: ", avg)
+
+
+# print("Input some integers to calculate their sum and average. Input 0 to exit.")
+
+# count = 0
+# sum = 0.0
+# number = 1
+
+# while number != 0:
+# 	number = int(input("Enter number: "))
+# 	sum = sum + number
+# 	count += 1
+
+# if count == 0:
+# 	print("Input some numbers")
+# else:
+# 	print("Average and Sum of the above numbers are: ", sum / (count-1),"and",  sum)
+
+
+

0af76e807e16206ea83fde1bec1da9ed73a2661f
Stefan Stoilkov <stefanstoilkov02@gmail.com>
2022-09-22 16:01:29 +0300

    Create 7.py

---
+++ LabExercises/lab_upr_1+2/7.py
@@ -0,0 +1,7 @@
+'''Da se sastavi programa, koqto obrshta cifrite
+na dadeno chislo:'''
+# primer: 76542  da se izvede 24567
+
+num = input("Enter num: ")
+for i in range(len(num) -1, -1, -1):
+    print(num[i], end='')
